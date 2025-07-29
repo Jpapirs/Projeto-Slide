6272623f7ebebf6b85730c343d3345c8d70c39f2
Jpapirs <joao.pedro.apsilva@gmail.com>
2025-07-28 21:01:41 -0300

    Jvascript aprimorado

--- js/slide.js
+++ js/slide.js
@@ -10,7 +10,7 @@ export default class Slide {
   }
 
   updatePosition(clientX) {
-    this.dist.movement = (this.dist.startX - clientX) *1.5;
+    this.dist.movement = (this.dist.startX - clientX) * 1.5;
     console.log(this.dist.movement);
     return this.dist.finalsPosition - this.dist.movement;
   }
@@ -21,21 +21,34 @@ export default class Slide {
   }
 
   onStart(event) {
-    event.preventDefault();
-    this.dist.startX = event.clientX;
-    this.wrapper.addEventListener("mousemove", this.onMove);
+    let moveType;
+    if (event.type === "mousedown") {
+      event.preventDefault();
+      this.dist.startX = event.clientX;
+      moveType = "mousemove";
+    } else {
+      this.dist.startX = event.changedTouches[0].clientX;
+      moveType = "touchmove";
+    }
+
+    this.wrapper.addEventListener(moveType, this.onMove);
   }
   onMove(event) {
-    const finalPosition = this.updatePosition(event.clientX);
+    const pointerPosition =
+      event.type === "mousemove" ? event.clientX : event.changedTouches[0].cli;
+    const finalPosition = this.updatePosition(pointerPosition);
     this.moveSlide(finalPosition);
   }
   onEnd(event) {
-    this.wrapper.removeEventListener("mousemove", this.onMove);
+    const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove'
+    this.wrapper.removeEventListener(movetype, this.onMove);
     this.dist.finalsPosition = this.dist.movePosition;
   }
   addSlideEvents() {
     this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("touchstart", this.onStart);
     this.wrapper.addEventListener("mouseup", this.onEnd);
+    this.wrapper.addEventListener("touchend", this.onEnd);
   }
 
   bindEvents() {
